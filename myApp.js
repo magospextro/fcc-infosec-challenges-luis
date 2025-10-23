c493fdcabea5f5f98607c7ff72e1cbf61d0e2ecc
root <root@dimensiontech.mago_spextro>
2025-10-22 23:10:55 -0300

    Actualizo código para reto helmet.hidePoweredBy()

--- myApp.js
+++ myApp.js
@@ -44,20 +44,4 @@ module.exports = app;
 
 
 
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-
-app.use('/_api', api);
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
-
-module.exports = app;
-
 
